4fd49ba7081bacc251656af5feba4eb40f468e3b
Daryl <daryl@Isaacs-MacBook-Pro.local>
2025-07-30 16:13:49 -0700

    changed github auth to google auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,13 +1,13 @@
 import { convexAuth } from "@convex-dev/auth/server";
-import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 import { ResendOTP } from "./otp/ResendOTP";
 
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [
     ResendOTP,
-    GitHub({
+    Google({
       authorization: {
-        params: { scope: "user:email" },
+        params: { scope: "openid email profile" },
       },
     }),
   ],
